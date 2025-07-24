4dce506402aed21ed2f4878984ba541a43c3b405
Abhay Kumar <154663062+Abhay91kumar@users.noreply.github.com>
2025-07-24 18:57:53 +0530

    Update Server.js

--- Server/Server.js
+++ Server/Server.js
@@ -14,6 +14,47 @@ app.use(cookiesParser());
 app.listen(Port, () => {
     console.log('Server is Running....')
 })
+const express = require('express');
+const mongoose = require('mongoose');
+const cookiesParser=require('cookie-parser')
+
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+const Port = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookiesParser());
+
+app.listen(Port, () => {
+    console.log('Server is Running....')
+})
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/uploadRouter'))
+
+//Connect MongoDB
+
+const uri = process.env.URI;
+//console.log("Env",process.env.URL)
+mongoose.connect(uri, {
+    //  useCreateIndex: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('✅ Connected to MongoDB');
+}).catch(err => {
+    console.error('❌ MongoDB connection failed:', err.message);
+})
+
 
 
 
