aebb00c2158d6bb41625b217eae218e4432ae2dc
Abhay Kumar <154663062+Abhay91kumar@users.noreply.github.com>
2025-07-30 13:08:58 +0530

    Update FashionAPI.js

--- client/src/api/FashionAPI.js
+++ client/src/api/FashionAPI.js
@@ -7,7 +7,7 @@ const FashionAPI = () => {
 
     const getFashions = async () => {
         try {
-            const res = await axios.get('https://e-commerce-2-zt8t.onrender.com/api/fashion');
+            const res = await axios.get('https://e-commerce-2-zt8t.onrender.com/api/fashion',{ withCredentials: true });
             // console.log('Fashion:', res.data);
             setFashions(res.data.fashions); 
         } catch (err) {
